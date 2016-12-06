6e1691c3af71b450d90ee4d6dbbacef11590e71b
Adrien Toubiana <adrientoub@hotmail.com>
2016-12-07 00:42:31 +0100

    raytracer: triangle intersection now works correctly

--- raytracer/src/triangle.cc
+++ raytracer/src/triangle.cc
@@ -8,25 +8,55 @@ Triangle::Triangle(const Vector3& a, const Vector3& b, const Vector3& c,
       : Shape(a, attr, color), b(b), c(c)
 {}
 
+Vector3 Triangle::normal_vect(const Vector3&) const
+{
+  const Vector3& a = pos;
+  Vector3 ab = b - a;
+  Vector3 ac = c - a;
+  return ab * ac;
+}
+
 Vector3 Triangle::intersect(const Ray& ray)
 {
-  Vector3& a = pos;
-  Vector3 ab = (b - a).normalize();
-  Vector3 ac = (c - a).normalize();
-  Vector3 n = ab * ac;
-  double d = -1 * n.dot_product(a);
-  double t0 = (n.dot_product(ray.position) + d)
-               * (-1 / n.dot_product(ray.direction));
-  Vector3 p = ray.position + ray.direction * t0;
-  Vector3 ap = (p - a).normalize();
-  double s = ap.dot_product(ac);
-  double r = ap.dot_product(ab);
+  // Argument is ignored
+  Vector3 n = normal_vect(ray.position);
+  const Vector3& a = pos;
 
-  if ((0 <= r && r <= 1) || (0 <= s && s <= 1)
-      || (0 <= (r + s) && (r + s) <= 1))
-    return p;
-  else
+  double n_dot_direction = n.dot_product(ray.direction);
+  if (std::abs(n_dot_direction) < 0.0001)
     return Vector3();
+  double d = n.dot_product(a);
+  double t0 = (n.dot_product(ray.position) + d)
+               / n_dot_direction;
+
+  // Triangle is behind the ray
+  if (t0 < 0)
+    return Vector3();
+
+  Vector3 p = ray.position + ray.direction * t0;
+
+  // perpendicular to triangle plane
+  Vector3 C;
+
+  Vector3 ab = b - a;
+  Vector3 ap = p - a;
+  C = ab * ap;
+  if (n.dot_product(C) < 0)
+    return Vector3();
+
+  Vector3 bc = c - b;
+  Vector3 bp = p - b;
+  C = bc * bp;
+  if (n.dot_product(C) < 0)
+    return Vector3();
+
+  Vector3 ca = a - c;
+  Vector3 cp = p - c;
+  C = ca * cp;
+  if (n.dot_product(C) < 0)
+    return Vector3();
+
+  return p;
 }
 
 std::istream& operator>>(std::istream& is, Triangle& triangle)
@@ -40,14 +70,6 @@ std::ostream& operator<<(std::ostream& os, const Triangle& triangle)
   return os << "Triangle: Pos: " << triangle.pos << " " << triangle.b << " "
             << triangle.c << " " << triangle.attr << " Color: "
             << triangle.color;
-}
-
-Vector3 Triangle::normal_vect(const Vector3&) const
-{
-  const Vector3& a = pos;
-  Vector3 ab = b - a;
-  Vector3 ac = c - a;
-  return ab * ac;
 }
 
 std::ostream& Triangle::display(std::ostream& os) const
