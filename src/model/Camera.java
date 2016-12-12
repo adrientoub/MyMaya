b5518efe28355ed3b694f5fd290132bdca0a724e
Adrien Toubiana <adrientoub@hotmail.com>
2016-12-12 23:13:13 +0100

    modeler: now exports camera rotation in the scene

--- src/model/Camera.java
+++ src/model/Camera.java
@@ -1,5 +1,6 @@
 package model;
 
+import javafx.geometry.Point3D;
 import javafx.scene.transform.Rotate;
 
 /**
@@ -37,9 +38,37 @@ public class Camera {
         camera.setTranslateZ(camera.getTranslateZ() + movement);
     }
 
+    private Point3D rotate(Point3D point, Point3D axis, double angle) {
+        double costheta = Math.cos(Math.toRadians(angle));
+        double sintheta = Math.sin(Math.toRadians(angle));
+
+        double r00 = costheta + Math.pow(axis.getX(), 2) * (1 - costheta);
+        double r01 = axis.getX() * axis.getY() * (1 - costheta) - axis.getZ() * sintheta;
+        double r02 = axis.getX() * axis.getZ() * (1 - costheta) + axis.getY() * sintheta;
+
+        double r10 = axis.getX() * axis.getY() * (1 - costheta) + axis.getZ() * sintheta;
+        double r11 = costheta + Math.pow(axis.getY(), 2) * (1 - costheta);
+        double r12 = axis.getY() * axis.getZ() * (1 - costheta) - axis.getX() * sintheta;
+
+        double r20 = axis.getX() * axis.getZ() * (1 - costheta) - axis.getY() * sintheta;
+        double r21 = axis.getY() * axis.getZ() * (1 - costheta) + axis.getX() * sintheta;
+        double r22 = costheta + Math.pow(axis.getZ(), 2) * (1 - costheta);
+
+        return new Point3D(r00 * point.getX() + r01 * point.getY() + r02 * point.getZ(),
+                r10 * point.getX() + r11 * point.getY() + r12 * point.getZ(),
+                r20 * point.getX() + r21 * point.getY() + r22 * point.getZ());
+    }
+
     @Override
     public String toString() {
-        // TODO: Find u and v vectors
-        return "camera " + Shape.getPositionString(camera) + " 1 0 0 0 -1 0";
+        Point3D u = new Point3D(1, 0, 0);
+        u = rotate(u, rotateX.getAxis(), rotateX.getAngle());
+        u = rotate(u, rotateY.getAxis(), rotateY.getAngle());
+
+        Point3D v = new Point3D(0, 1, 0);
+        v = rotate(v, rotateX.getAxis(), rotateX.getAngle());
+        v = rotate(v, rotateY.getAxis(), rotateY.getAngle());
+
+        return "camera " + Shape.getPositionString(camera) + " " + u.getX() + " " + u.getY() + " " + u.getZ() + " " + v.getX() + " " + v.getY() + " " + v.getZ();
     }
 }
