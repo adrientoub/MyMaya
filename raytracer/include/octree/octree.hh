d4019e142700891623d03a50e531b682d52879b7
Adrien Toubiana <adrientoub@hotmail.com>
2017-02-09 03:00:27 +0100

    raytracer: use interesting part of the triangles when building octree

--- raytracer/include/octree/octree.hh
+++ raytracer/include/octree/octree.hh
@@ -13,7 +13,8 @@ class Octree: public Node
 public:
   virtual ~Octree() = default;
   std::vector<const Triangle*> intersect(const Ray& ray) const;
-  static Octree* build_octree(const std::vector<Triangle>& triangles,
+  template <typename T>
+  static Octree* build_octree(const T& triangles,
                               const std::array<Vector3, 2>& bounds,
                               const size_t depth = 3);
 
@@ -23,3 +24,12 @@ private:
   static const size_t optimal_by_node = 5;
   std::array<std::unique_ptr<Node>, 8> children;
 };
+
+std::vector<const Triangle*> find_all_triangles(const std::vector<Triangle>& triangles,
+                                                const std::array<Vector3, 2>& bounds);
+
+std::vector<const Triangle*> find_all_triangles(const std::vector<const Triangle*>& triangles,
+                                                const std::array<Vector3, 2>& bounds);
+
+
+#include "octree.hxx"
--- raytracer/src/octree/octree.cc
+++ raytracer/src/octree/octree.cc
@@ -1,5 +1,4 @@
 #include "octree/octree.hh"
-#include "octree/leaf.hh"
 #include "box.hh"
 
 #include <algorithm>
@@ -13,6 +12,17 @@ std::vector<const Triangle*> find_all_triangles(const std::vector<Triangle>& tri
   for (const Triangle& triangle: triangles)
     if (triangle.inside(bounds))
       out.push_back(&triangle);
+
+  return out;
+}
+
+std::vector<const Triangle*> find_all_triangles(const std::vector<const Triangle*>& triangles,
+                                                const std::array<Vector3, 2>& bounds)
+{
+  std::vector<const Triangle*> out;
+  for (const Triangle* triangle: triangles)
+    if (triangle->inside(bounds))
+      out.push_back(triangle);
 
   return out;
 }
@@ -37,43 +47,4 @@ void Octree::intersect(const Ray& ray,
     if (children[i])
       children[i]->intersect(ray, triangles);
   }
-}
-
-Octree* Octree::build_octree(const std::vector<Triangle>& triangles,
-                             const std::array<Vector3, 2>& bounds,
-                             const size_t depth)
-{
-  Octree* tree = new Octree();
-  std::copy(bounds.begin(), bounds.end(), tree->bounds.begin());
-
-  Vector3 mid = (bounds[1] - bounds[0]) / 2;
-
-  for (size_t i = 0; i < tree->children.size(); ++i)
-  {
-    std::array<Vector3, 2> bnds;
-    bnds[0] = Vector3(
-      bounds[0].getX() + (i & 1) * mid.getX(),
-      bounds[0].getY() + ((i & 2) >> 1) * mid.getY(),
-      bounds[0].getZ() + ((i & 4) >> 2) * mid.getZ()
-    );
-    bnds[1] = bnds[0] + mid;
-    std::vector<const Triangle*> part_triangles = find_all_triangles(triangles, bnds);
-
-    if (part_triangles.empty())
-      tree->children[i] = nullptr;
-    else
-    {
-      if (depth == 0 || part_triangles.size() < optimal_by_node)
-      {
-        std::unique_ptr<Leaf> leaf = std::make_unique<Leaf>();
-        leaf->triangles = part_triangles;
-        leaf->bounds = bnds;
-        tree->children[i] = std::move(leaf);
-      }
-      else
-        tree->children[i] = std::unique_ptr<Octree>(build_octree(triangles, bnds, depth - 1));
-    }
-  }
-
-  return tree;
 }
